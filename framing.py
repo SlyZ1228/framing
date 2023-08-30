39edc6f87eb107a6ecd9273df253705298b01d04
SlyZ1228 <zayanniaz@gmail.com>
2023-08-29 23:48:26 -0500

    updated format of matrix

--- framing.py
+++ framing.py
@@ -1,5 +1,4 @@
-from pybaseball import statcast, cache
-cache.enable() # Optional
+from pybaseball import statcast
 
 import datetime
 import pandas as pd
@@ -8,27 +7,42 @@ def re_matrix(year):
   count = 1
   ym, y1m, y2m = None, None, None
 
-  #loop for all three years
+  # loop for all three years
   for yr in [year, year - 1, year - 2]:
     season_start = datetime.datetime(yr, 3, 1)
     season_end = datetime.datetime(yr, 11, 1)
 
-    ### load season data w pybaseball
+    # load season data w pybaseball
     pitch_data = statcast(start_dt=season_start.strftime('%Y-%m-%d'),end_dt=season_end.strftime('%Y-%m-%d'))
 
-    ### create base state representations
+    # create base state representations
     for base in ['1b','2b','3b']:
       pitch_data.loc[pitch_data['on_'+base].notna(),'on_'+base] = int(base[0])
       pitch_data['on_'+base] = pitch_data['on_'+base].astype('str').replace('<NA>','_')
     pitch_data['base_state'] = pitch_data['on_1b']+' '+pitch_data['on_2b']+' '+pitch_data['on_3b']
 
-    ### determine number of run innings
+    # determine number of run innings
     pitch_data['start_inning_score'] = pitch_data['bat_score'].groupby([pitch_data['game_pk'],pitch_data['inning'],pitch_data['inning_topbot']]).transform('min')
     pitch_data['end_inning_score'] = pitch_data['bat_score'].groupby([pitch_data['game_pk'],pitch_data['inning'],pitch_data['inning_topbot']]).transform('max')
     pitch_data['inning_runs'] = pitch_data['end_inning_score'].sub(pitch_data['start_inning_score']).astype('int')
 
-    ### make the dataframe for the matrix
-    ### 3 outs x 8 base states
+    # formats the re matrix base states
+    from pandas.api.types import CategoricalDtype
+    base_state_cats = CategoricalDtype(
+        ['_ _ _', 
+         '1 _ _', 
+         '_ 2 _', 
+         '1 2 _',
+         '_ _ 3', 
+         '1 _ 3', 
+         '_ 2 3', 
+         '1 2 3'], 
+        ordered=True
+    )
+    pitch_data['base_state'] = pitch_data['base_state'].astype(base_state_cats)
+
+    # make the dataframe for the matrix
+    # 3 outs x 8 base states
     data = (pitch_data
                 .dropna(subset=['events']) # removes pitches that dont lead to pa change
                 .groupby(['game_year','base_state','outs_when_up']) # average runs for each base out state
@@ -51,12 +65,15 @@ def re_matrix(year):
 
     count += 1
 
+  # weight the re matrices
   ym = ym.mul(0.5)
   y1m = y1m.mul(0.25)
   y2m = y2m.mul(0.25)
 
+  # add em up
   final = ym.reset_index(level='game_year', drop=True).add(y1m.reset_index(level='game_year', drop=True).add(y2m.reset_index(level='game_year', drop=True)))
 
+  # save
   print(final)
   final.to_csv(f'RE24 Matrix - {year}.csv')
 
