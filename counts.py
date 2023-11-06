aa75ab0adb38ac5cf01b74b48c407361918c84c5
SlyZ1228 <zayanniaz@gmail.com>
2023-11-06 09:23:45 -0600

    fixed hopefully

--- counts.py
+++ counts.py
@@ -1,35 +1,33 @@
 import pandas as pd
-import numpy as np
 import PA
 
-# get the data
-re = pd.read_csv('data\RE24 Matrix - 2023.csv')
-PA = pd.read_csv('data\PA outcomes.csv')
-pitches = pd.read_csv('data\countdata.csv')
+def count_re():
+    # import data
+    PA = pd.read_csv('data\PA outcomes.csv')
+    pitches = pd.read_csv('data\pitchdata.csv')
 
-# drop unnecessary columns and eventless pitches and create a "count" column
-pitches = pd.concat([pitches['game_pk'], pitches['inning'], pitches['inning_topbot'], pitches['balls'], pitches['strikes'], pitches['events'], pitches['game_year'], pitches['at_bat_number']], axis=1)
-pitches['count'] = pitches['balls'].astype(str).str.cat(pitches['strikes'].astype(str), sep='-')
-pitches = pitches.set_index(['game_pk', 'inning', 'inning_topbot', 'at_bat_number', 'count'], drop=False)
-pitches = pitches.sort_index(level=['game_pk', 'inning', 'inning_topbot', 'at_bat_number', 'count'], ascending=[False, True, False, True, True]).drop(columns=['game_pk', 'inning', 'inning_topbot', 'at_bat_number', 'count'])
-pitches['events'] = pitches['events'].bfill()
-pitches = pitches.drop(columns=['balls', 'strikes'])
+    # drop unnecessary columns, create "count" column, and backfill events list
+    pitches = pd.concat([pitches['game_pk'], pitches['inning'], pitches['inning_topbot'], pitches['balls'], pitches['strikes'], pitches['events'], pitches['game_year'], pitches['at_bat_number']], axis=1)
+    pitches['count'] = pitches['balls'].astype(str).str.cat(pitches['strikes'].astype(str), sep='-')
+    pitches = pitches.set_index(['game_pk', 'inning', 'inning_topbot', 'at_bat_number', 'count'], drop=True).sort_index(level=['game_pk', 'inning', 'inning_topbot', 'at_bat_number', 'count'], ascending=[False, True, False, True, True]).drop(columns=['balls', 'strikes'])
+    pitches['events'] = pitches['events'].bfill()
 
-# map the run expectancy changes per event and then average them over counts
-map = PA.set_index('events')['change_re'].to_dict()
-pitches['count_values'] = pitches['events'].map(map)
-counts = pitches.reset_index().set_index(['count'], drop=True).sort_index(level=['count'], ascending=True)
-counts = pitches.groupby(['count'])['count_values'].mean()
+    # map the run expectancy changes by event and average them over the counts
+    map = PA.set_index('events')['change_re'].to_dict()
+    pitches['count_values'] = pitches['events'].map(map)
+    counts = pitches.reset_index().set_index(['count'], drop=True).sort_index(level=['count'], ascending=True)
+    counts = pitches.groupby(['count'])['count_values'].mean()
 
-# create a df with the data labeled by ball and strike count
-data = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2], [3, 0], [3, 1], [3, 2], [4, 2]]
-df = pd.DataFrame(data, columns=['balls', 'strikes'])
-df['count_values'] = counts.reset_index(drop=True).squeeze()
-df.drop(index=12, inplace=True)
+    # create a df with the data labeled by ball and strike count
+    data = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2], [3, 0], [3, 1], [3, 2], [4, 2]]
+    df = pd.DataFrame(data, columns=['balls', 'strikes'])
+    df['count_values'] = counts.reset_index(drop=True).squeeze()
 
-# pivot with strikes at top and balls on side
-counts = df.pivot(index='balls', columns='strikes', values='count_values').copy()
+    # pivot with strikes at top and balls on side
+    counts = df.drop(index=12).pivot(index='balls', columns='strikes', values='count_values').copy()
 
-print(counts)
+    # return and save data
+    print(counts)
+    counts.to_csv('data\counts.csv')
 
-counts.to_csv('data\counts.csv')
+count_re()
--- matrix.py
+++ matrix.py
@@ -3,6 +3,7 @@ cache.enable()
 
 import datetime
 import pandas as pd
+import numpy as np
 
 def re_matrix(year):
   count = 1
@@ -19,7 +20,7 @@ def re_matrix(year):
 
     # load season data w pybaseball
     pitch_data = statcast(start_dt=season_start.strftime('%Y-%m-%d'),end_dt=season_end.strftime('%Y-%m-%d'))
-
+    
     # create base state representations
     for base in ['1b','2b','3b']:
       pitch_data.loc[pitch_data['on_'+base].notna(),'on_'+base] = int(base[0])
@@ -44,12 +45,12 @@ def re_matrix(year):
         ordered=True
     )
     pitch_data['base_state'] = pitch_data['base_state'].astype(base_state_cats)
-    pitch_data.to_csv('data\pitchdata.csv')
+    
 
     # make the dataframe for the matrix
     # 3 outs x 8 base states
     data = (pitch_data
-                .dropna(subset=['events']) # removes pitches that dont lead to pa change
+                .dropna(subset=['events']) # removes pitch_data that dont lead to pa change
                 .groupby(['game_year','base_state','outs_when_up'], observed=False) # average runs for each base out state
                 ['inning_runs']
                 .mean()
@@ -63,21 +64,27 @@ def re_matrix(year):
     match count:
       case 1:
         ym = data
+        df = pitch_data.copy()
       case 2: 
         y1m = data
+        df = pd.concat([df, pitch_data])
       case 3: 
         y2m = data
+        df = pd.concat([df, pitch_data])
 
     count += 1
+
+  df['game_year'] = df['game_year'].replace(2021, np.NaN).replace(2022, np.NaN)
+  df = df.dropna(subset='game_year')
+  df.to_csv('data\pitchdata.csv')
 
   # weight the re matrices
   ym = ym.mul(0.5)
   y1m = y1m.mul(0.25)
   y2m = y2m.mul(0.25)
 
-  # add em up
+  # add them up
   final = ym.reset_index(level='game_year', drop=True).add(y1m.reset_index(level='game_year', drop=True).add(y2m.reset_index(level='game_year', drop=True)))
-  # final.to_csv(f"data/RE24 Matrix - {year}.csv")
 
   final.to_csv('data\RE24 Matrix - 2023.csv')
 
